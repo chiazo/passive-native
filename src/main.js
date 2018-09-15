07c9fa807ecb8974bfa916fcafce3b8d5fd9055e
chiazo <chiazo@live.unc.edu>
2018-09-15 16:57:14 -0400

    chrome.storage code

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 var wordMap = new Object();
+
+chrome.storage.sync.set({key: value}, function() {
+    console.log('Value is set to ' + value);
+  });
+
+  chrome.storage.sync.get(['key'], function(result) {
+    console.log('Value currently is ' + result.key);
+  });
