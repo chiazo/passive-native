61f3690735a36517ae1eadce4cb3053883e2c43c
chiazo <chiazo@live.unc.edu>
2018-09-15 20:32:04 -0400

    something

--- src/main.js
+++ src/main.js
@@ -1,9 +1,29 @@
-var wordMap = new Object();
 
-chrome.storage.sync.set({key: value}, function() {
+var igboWords = {};
+let wordMap ={};
+
+/* chrome.storage.sync.set({key: value}, function() {
     console.log('Value is set to ' + value);
   });
 
   chrome.storage.sync.get(['key'], function(result) {
     console.log('Value currently is ' + result.key);
+  }); */
+
+  d3.csv("/Users/chiazo/Desktop/igboWords.csv", function(data) {
+     igboWords = data;
   });
+
+ 
+    for (let i = 0; i < result.length; i++){
+       let wordMap = data[i];
+    }   
+    
+  
+
+
+  // figure out how to update data when user exits browser
+
+  // store words as an array of objects & sync for first time
+
+  // dump words that are already seen in the same session so they wont be seen again until the next session
