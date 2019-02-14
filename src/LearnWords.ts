4e0202b88df9a8325d10abd0df3915e0e328e6d1
chiazo <chiazo@live.unc.edu>
2019-02-14 12:40:15 -0500

    combining mojo & irene's html

--- src/LearnWords.ts
+++ src/LearnWords.ts
@@ -1,21 +1,42 @@
 import { WordImpl } from "./WordImpl";
+import { wordList } from "./main";
 
 export class LearnWords extends WordImpl {
     private totalAttempts: number;
     private correctAttempts: number;
     private percentCorrect: number;
 
-    constructor(term: string, translation: string, totalAttempts: number, correctAttempts: number, category?: string) {
+    constructor(term: string, translation: string, category?: string) {
         super(term, translation, category);
-        this.totalAttempts = totalAttempts;
-        this.correctAttempts = correctAttempts;
+        this.totalAttempts = 0;
+        this.correctAttempts = 0;
 
-        if (totalAttempts !== 0) {
+        if (this.totalAttempts !== 0) {
             this.percentCorrect = this.correctAttempts / this.totalAttempts;
         } else {
             this.percentCorrect = 0;
         }
      }
+
+    getPercentCorrect(): number {
+        return this.percentCorrect;
+    }
+
+    getTotalAttempts(): number {
+        return this.totalAttempts;
+    }
+
+    getCorrectAttempts(): number {
+        return this.correctAttempts;
+    }
+
+    setCorrectAttempts(correct: boolean) {
+        this.totalAttempts++;
+        if (correct) {
+            this.correctAttempts++;
+        }
+    }
+
 
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,46 @@
 import { WordImpl } from '../src/WordImpl';
 import { Word } from '../src/Word';
 import * as igbowords from '../data/igbowords.json';
+import { LearnWords } from './LearnWords';
 
-let wordList: WordImpl[] = [];
+export let wordList: WordImpl[] = [];
 
 for(var i in igbowords) {
     var item = igbowords[i];
     wordList[wordList.length] = new WordImpl(item.term, item.translation, item.category);
 }
 
-console.log(wordList);
+export let learntWords: LearnWords[] = [];
+
+for (let i = 0; i < wordList.length; i++) {
+    learntWords[learntWords.length] = new LearnWords(wordList[i].term, wordList[i].translation, wordList[i].category);
+}
+
+var highestFreq: LearnWords[] = [];
+var highFreq: LearnWords[] = [];
+var medFreq: LearnWords[] = [];
+var lowFreq: LearnWords[] = [];
+var lowestFreq: LearnWords[] =[];
+
+function sortWords(){
+   for(let i = 0; i < learntWords.length; i++){
+       if(learntWords[i].getPercentCorrect() < .20){
+           highestFreq.push(learntWords[i]);
+       
+        } else if(learntWords[i].getPercentCorrect() < .40){
+           highFreq.push(learntWords[i]);
+       
+        } else if(learntWords[i].getPercentCorrect() < .60){
+           medFreq.push(learntWords[i]);
+       
+        } else if(learntWords[i].getPercentCorrect() < .80){
+           lowFreq.push(learntWords[i]);
+       
+        } else {
+           lowestFreq.push(learntWords[i]);
+       }
+   }
+
+   
+}
+
